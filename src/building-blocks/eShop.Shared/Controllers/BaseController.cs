2bb385d4d53b05b1fb8bcd05a068a9370d8a0f58
Vinh <hoangdvinh68@gmail.com>
2023-12-13 15:30:20 +0700

    init base structure

---
+++ src/building-blocks/eShop.Shared/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.Shared.Controllers;
+
+[ApiController]
+[Route("api/v1[controller]/[action]")]
+public class BaseController : ControllerBase
+{
+    protected readonly IMediator Mediator;
+
+    public BaseController(IMediator mediator)
+    {
+        Mediator = mediator;
+    }
+}
